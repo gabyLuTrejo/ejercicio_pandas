b4e9f64c453da68bb4ce9df51983ac65a3307b58
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-18 10:37:22 -0500

    Boton origen funcionando

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function origen(){
+  var textoIzquierda = document.getElementById("textoIzq");
+  if (textoIzquierda.style.display == "none") {
+    textoIzquierda.style.display = "inline-block"
+  } else {
+      textoIzquierda.style.display = "none"
+  }
+}
