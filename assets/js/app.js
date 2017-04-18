d8ea90054f109aec4900f077828fe422f5a1d867
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-18 17:02:37 -0500

    Boton Restaurar Fotos habilitado

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,14 +24,25 @@ function extincion(){
 
 
 var span = document.getElementsByClassName("cerrar");
+var cantidadFotos = span.length;
 
-for(var i = 0; i<span.length; i++){
+for(var i=0; i<cantidadFotos; i++){
   span[i].addEventListener("click", fotos);
 }
 
 function fotos(){;
   var eliminarFoto = this.getAttribute("name");
-  console.log(eliminarFoto);
   this.style.display = "none";
   document.getElementById(eliminarFoto).style.display = "none";
+}
+
+var botonRestaurar = document.getElementById("botonRestaurar");
+botonRestaurar.addEventListener("click", restaurarFotos);
+
+function restaurarFotos(){
+  for(var i = 0; i<cantidadFotos; i++){
+    var foto = span[i].getAttribute("name");
+    span[i].style.display = "inline-block";
+    document.getElementById(foto).style.display = "inline-block";
+  }
 }
