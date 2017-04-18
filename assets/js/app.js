84643a8a0050e7a0f5cbd750c4a01011e18ea4aa
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-18 16:51:36 -0500

    X: eliminar imagenes habilitado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,37 @@
+var botonOrigen = document.getElementById("botonOrigen");
+botonOrigen.addEventListener("click", origen);
+var botonExtincion = document.getElementById("botonExtincion");
+botonExtincion.addEventListener("click", extincion);
+
 function origen(){
+  console.log("DDD");
   var textoIzquierda = document.getElementById("textoIzq");
-  if (textoIzquierda.style.display == "none") {
-    textoIzquierda.style.display = "inline-block"
+  if (textoIzquierda.style.visibility == "hidden") {
+    textoIzquierda.style.visibility = "visible"
   } else {
-      textoIzquierda.style.display = "none"
+      textoIzquierda.style.visibility = "hidden"
   }
 }
 
 function extincion(){
   var textoDerecha = document.getElementById("textoDer");
-  if (textoDerecha.style.display == "none") {
-    textoDerecha.style.display = "inline-block"
+  if (textoDerecha.style.visibility == "hidden") {
+    textoDerecha.style.visibility = "visible"
   } else {
-      textoDerecha.style.display = "none"
+      textoDerecha.style.visibility = "hidden"
   }
+}
+
+
+var span = document.getElementsByClassName("cerrar");
+
+for(var i = 0; i<span.length; i++){
+  span[i].addEventListener("click", fotos);
+}
+
+function fotos(){;
+  var eliminarFoto = this.getAttribute("name");
+  console.log(eliminarFoto);
+  this.style.display = "none";
+  document.getElementById(eliminarFoto).style.display = "none";
 }
